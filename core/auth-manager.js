01bec13a1cd72ead5411a332acbe8324db9253c1
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 07:41:39 +0530

    Implement authentication methods in auth-manager

--- core/auth-manager.js
+++ core/auth-manager.js
@@ -1,1 +1,47 @@
-// auth-manager.js
+window.MAP = window.MAP || {};
+
+window.MAP.Auth = {
+
+    async getSession(){
+
+        const {
+            data,
+            error
+        } = await window.MAP.supabase.auth.getSession();
+
+        if(error){
+            console.error(error);
+            return null;
+        }
+
+        return data.session;
+    },
+
+    async login(email,password){
+
+        const {
+            data,
+            error
+        } = await window.MAP.supabase.auth.signInWithPassword({
+            email,
+            password
+        });
+
+        if(error){
+
+            alert(error.message);
+
+            return false;
+        }
+
+        return true;
+    },
+
+    async logout(){
+
+        await window.MAP.supabase.auth.signOut();
+
+        location.reload();
+    }
+
+};
