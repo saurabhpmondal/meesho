5fab653e0b8fe863281b9da6f0e003da4ad451c9
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 07:37:12 +0530

    Update supabase-config.js

--- config/supabase-config.js
+++ config/supabase-config.js
@@ -1,4 +1,6 @@
-window.SUPABASE_CONFIG = {
-    url: "https://zhdixvysmgwodldlgmtf.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZGl4dnlzbWd3b2RsZGxnbXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4OTUxNjEsImV4cCI6MjA5NzQ3MTE2MX0.wbusg59AtCwPef2GlmFavljuw7UztAB26Fgl5gNX9sI"
+window.MAP = window.MAP || {};
+
+window.MAP.config = {
+    supabaseUrl: "https://zhdixvysmgwodldlgmtf.supabase.co",
+    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZGl4dnlzbWd3b2RsZGxnbXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4OTUxNjEsImV4cCI6MjA5NzQ3MTE2MX0.wbusg59AtCwPef2GlmFavljuw7UztAB26Fgl5gNX9sI"
 };
