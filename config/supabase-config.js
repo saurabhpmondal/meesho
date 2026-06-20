261aca5b921fb75aa6fabb029d07992fef5097b5
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 07:31:59 +0530

    Update supabase-config.js

--- config/supabase-config.js
+++ config/supabase-config.js
@@ -1,1 +1,4 @@
-// supabase-config.js
+window.SUPABASE_CONFIG = {
+    url: "https://zhdixvysmgwodldlgmtf.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZGl4dnlzbWd3b2RsZGxnbXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4OTUxNjEsImV4cCI6MjA5NzQ3MTE2MX0.wbusg59AtCwPef2GlmFavljuw7UztAB26Fgl5gNX9sI"
+};
