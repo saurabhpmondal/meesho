87139e4e0fa6bf29b94f07246f2b11ee43a5cb12
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 08:35:30 +0530

    Update data-store.js

--- core/data-store.js
+++ core/data-store.js
@@ -1,3 +1,5 @@
+window.MAP = window.MAP || {};
+
 window.MAP.DataStore = {
 
     sales: [],
@@ -6,10 +8,34 @@ window.MAP.DataStore = {
 
     master: [],
 
-    loaded: {
-        sales:false,
-        ads:false,
-        master:false
+    metadata: {
+
+        salesRows: 0,
+
+        adsRows: 0,
+
+        masterRows: 0,
+
+        lastRefresh: null,
+
+        refreshStatus: "Not Loaded"
+
+    },
+
+    updateMetadata(){
+
+        this.metadata.salesRows =
+            this.sales.length;
+
+        this.metadata.adsRows =
+            this.ads.length;
+
+        this.metadata.masterRows =
+            this.master.length;
+
+        this.metadata.lastRefresh =
+            new Date().toISOString();
+
     }
 
 };
