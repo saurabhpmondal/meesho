80f748c0d7e81ba27f5e6bb07a2f76dcf292978f
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 08:38:38 +0530

    Implement MasterLoader to load data from Google Sheets

--- data/loaders/master-loader.js
+++ data/loaders/master-loader.js
@@ -1,1 +1,43 @@
-// master-loader.js
+window.MAP = window.MAP || {};
+
+window.MAP.MasterLoader = {
+
+    GID: "1659451695",
+
+    async load(){
+
+        try{
+
+            window.MAP.DataStore
+                .metadata
+                .refreshStatus =
+                "Loading Master Sheet";
+
+            const data =
+                await window.MAP
+                .GoogleSheetService
+                .fetchSheet(
+                    this.GID,
+                    "master_cache"
+                );
+
+            window.MAP.DataStore.master =
+                data;
+
+            window.MAP.DataStore
+                .updateMetadata();
+
+            return data;
+
+        }
+        catch(error){
+
+            console.error(error);
+
+            return [];
+
+        }
+
+    }
+
+};
