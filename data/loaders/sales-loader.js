295803216f217f1cb738d430961d6e35fc3c64db
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 08:38:04 +0530

    Implement SalesLoader for fetching sales data
    Added SalesLoader to manage loading sales data from Google Sheets.

--- data/loaders/sales-loader.js
+++ data/loaders/sales-loader.js
@@ -1,1 +1,43 @@
-// sales-loader.js
+window.MAP = window.MAP || {};
+
+window.MAP.SalesLoader = {
+
+    GID: "328436164",
+
+    async load(){
+
+        try{
+
+            window.MAP.DataStore
+                .metadata
+                .refreshStatus =
+                "Loading Sales Sheet";
+
+            const data =
+                await window.MAP
+                .GoogleSheetService
+                .fetchSheet(
+                    this.GID,
+                    "sales_cache"
+                );
+
+            window.MAP.DataStore.sales =
+                data;
+
+            window.MAP.DataStore
+                .updateMetadata();
+
+            return data;
+
+        }
+        catch(error){
+
+            console.error(error);
+
+            return [];
+
+        }
+
+    }
+
+};
