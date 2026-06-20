10cf0ef8eebc8fe7daf2ccd8922f1c906d018a29
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 08:53:12 +0530

    Update sales-repository.js

--- data/repositories/sales-repository.js
+++ data/repositories/sales-repository.js
@@ -1,1 +1,72 @@
-// sales-repository.js
+window.MAP = window.MAP || {};
+
+window.MAP.SalesRepository = {
+
+    getRows(){
+
+        return (
+            window.MAP.DataStore
+            .sales || []
+        );
+
+    },
+
+    getUnits(){
+
+        return this
+            .getRows()
+            .reduce(
+
+                (sum,row)=>
+
+                    sum +
+
+                    Number(
+                        row.quantity || 0
+                    ),
+
+                0
+
+            );
+
+    },
+
+    getGMV(){
+
+        return this
+            .getRows()
+            .reduce(
+
+                (sum,row)=>
+
+                    sum +
+
+                    Number(
+                        row[
+                        "supplier_discounted_price_(incl_gst_and_commision)"
+                        ] || 0
+                    ),
+
+                0
+
+            );
+
+    },
+
+    getASP(){
+
+        const units =
+            this.getUnits();
+
+        if(!units){
+            return 0;
+        }
+
+        return (
+            this.getGMV() /
+            units
+        );
+
+    }
+
+};
