1e573b2f2dd2167e0a8bdbb6c00a703aa782074d
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 07:38:51 +0530

    Initialize Supabase client in supabase-service.js

--- services/supabase-service.js
+++ services/supabase-service.js
@@ -1,1 +1,8 @@
-// supabase-service.js
+window.MAP = window.MAP || {};
+
+window.MAP.supabase = supabase.createClient(
+    window.MAP.config.supabaseUrl,
+    window.MAP.config.supabaseAnonKey
+);
+
+console.log("Supabase Initialized");
