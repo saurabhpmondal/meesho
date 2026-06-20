ba0af910fcce2082d0e87cd80798fd190c8fe197
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 08:53:32 +0530

    Update ads-repository.js

--- data/repositories/ads-repository.js
+++ data/repositories/ads-repository.js
@@ -1,1 +1,70 @@
-// ads-repository.js
+window.MAP = window.MAP || {};
+
+window.MAP.AdsRepository = {
+
+    getRows(){
+
+        return (
+            window.MAP.DataStore
+            .ads || []
+        );
+
+    },
+
+    getAdSpend(){
+
+        return this
+            .getRows()
+            .reduce(
+
+                (sum,row)=>
+
+                    sum +
+
+                    Number(
+                        row.ads_spend || 0
+                    ),
+
+                0
+
+            );
+
+    },
+
+    getRevenue(){
+
+        return this
+            .getRows()
+            .reduce(
+
+                (sum,row)=>
+
+                    sum +
+
+                    Number(
+                        row.revenue || 0
+                    ),
+
+                0
+
+            );
+
+    },
+
+    getROI(){
+
+        const spend =
+            this.getAdSpend();
+
+        if(!spend){
+            return 0;
+        }
+
+        return (
+            this.getRevenue() /
+            spend
+        );
+
+    }
+
+};
