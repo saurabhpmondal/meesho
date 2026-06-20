00767a420d4a14de5167fa5a3c7e3ee4a7d1b8eb
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 08:38:23 +0530

    Update ads-loader.js

--- data/loaders/ads-loader.js
+++ data/loaders/ads-loader.js
@@ -1,1 +1,43 @@
-// ads-loader.js
+window.MAP = window.MAP || {};
+
+window.MAP.AdsLoader = {
+
+    GID: "8523756",
+
+    async load(){
+
+        try{
+
+            window.MAP.DataStore
+                .metadata
+                .refreshStatus =
+                "Loading Ads Sheet";
+
+            const data =
+                await window.MAP
+                .GoogleSheetService
+                .fetchSheet(
+                    this.GID,
+                    "ads_cache"
+                );
+
+            window.MAP.DataStore.ads =
+                data;
+
+            window.MAP.DataStore
+                .updateMetadata();
+
+            return data;
+
+        }
+        catch(error){
+
+            console.error(error);
+
+            return [];
+
+        }
+
+    }
+
+};
