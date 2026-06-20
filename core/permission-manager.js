8deb44d86cb4e534964a13e0631c8027f40376ec
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-20 07:50:11 +0530

    Update permission-manager.js

--- core/permission-manager.js
+++ core/permission-manager.js
@@ -1,1 +1,26 @@
-// permission-manager.js
+window.MAP = window.MAP || {};
+
+window.MAP.PermissionManager = {
+
+    async getPermissions(roleId){
+
+        const { data, error } =
+            await window.MAP.supabase
+            .from("role_permissions")
+            .select(`
+                permissions (
+                    permission_key
+                )
+            `)
+            .eq("role_id", roleId);
+
+        if(error){
+            throw error;
+        }
+
+        return (data || [])
+            .map(x => x.permissions?.permission_key)
+            .filter(Boolean);
+    }
+
+};
